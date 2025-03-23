132efe911a39675fa7f0bd27e709d7f763ecf4de
IT21361036 <it21361036@my.sliit.lk>
2025-03-23 21:51:56 +0530

    changes in .env file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env
+dotenv.config();
 
 const app = express();
 
@@ -10,10 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/product-management', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
 
 // Routes
 app.use('/api', productRoutes);
